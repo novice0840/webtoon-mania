10417163baf7ecf66417af7132aff9ef5ba2d4b9
lotus9630 <68753108+lotus9630@users.noreply.github.com>
2023-01-22 06:03:10 +0900

    typescript, context, reducer exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,13 @@
-import { useState } from "react";
-import "./styles.css";
-import Board from "components/Board";
+import * as React from "react";
+import { AppProvider } from "./context";
+import Products from "./Products";
+import List from "./List";
 
 export default function App() {
-    const [history, setHistory] = useState([Array(9).fill(null)]);
-    const [currentMove, setCurrentMove] = useState(0);
-    const xIsNext = currentMove % 2 === 0;
-    const currentSquares = history[currentMove];
-
-    function handlePlay(nextSquares: string[]) {
-        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-        setHistory(nextHistory);
-        setCurrentMove(nextHistory.length - 1);
-    }
-
-    function jumpTo(nextMove: number) {
-        setCurrentMove(nextMove);
-    }
-
-    const moves = history.map((squares, move) => {
-        let description;
-        if (move > 0) {
-            description = "Go to move #" + move;
-        } else {
-            description = "Go to game start";
-        }
-        return (
-            <li key={move}>
-                <button onClick={() => jumpTo(move)}>{description}</button>
-            </li>
-        );
-    });
-
-    return (
-        <div className="game">
-            <div className="game-board">
-                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-            </div>
-            <div className="game-info">
-                <ol>{moves}</ol>
-            </div>
-        </div>
-    );
+  return (
+    <AppProvider>
+      <Products />
+      <List />
+    </AppProvider>
+  );
 }
--- src/components/Square.tsx
+++
@@ -1,7 +0,0 @@
-export default function Square({ value, onSquareClick }: { value: string; onSquareClick: () => void }): JSX.Element {
-    return (
-        <button className="square" onClick={onSquareClick}>
-            {value}
-        </button>
-    );
-}
