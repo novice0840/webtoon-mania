3930a959fb149f5aab16485976334993919cbeda
novice0840 <magnolia0840@gmail.com>
2023-07-25 22:58:17 +0900

    github action test

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Worldsadd2!231312';
+    return 'github action test';
   }
 }
