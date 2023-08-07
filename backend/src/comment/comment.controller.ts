a88c29da8357ce24cac8500720b95fba8b9094e7
novice0840 <magnolia0840@gmail.com>
2023-08-08 03:41:33 +0900

    nestjs websocket module 추가

--- backend/src/comment/comment.controller.ts
+++ backend/src/comment/comment.controller.ts
@@ -17,11 +17,23 @@ import { CreateCommentDto } from './dto/create-dto';
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
+  // 특정 웹툰의 댓글 보기
+  @Get('webtoon/:webtoonId')
+  getWebtoonComments(@Param('webtoonId') webtoonId) {
+    return this.commentService.getWebtoonComments(webtoonId);
+  }
+
+  // 내 댓글 보기
+  @Get('my')
+  getUserComments(@Request() req) {
+    return this.commentService.getUserComments(req.user.id);
+  }
+
   // 댓글 쓰기
   @Post()
   createComment(@Body() createCommentDto: CreateCommentDto, @Request() req) {
     return this.commentService.createComment(
-      req.user,
+      req.user.id,
       createCommentDto.webtoonId,
       createCommentDto.content,
     );
@@ -29,19 +41,19 @@ export class CommentController {
 
   // 댓글을 삭제하는 기능 자기 자신의 댓글만 삭제 가능
   @Delete(':commentId')
-  deleteComment(@Param('commentId') commentId: string) {
-    return this.commentService.deleteComment(commentId);
+  deleteComment(@Param('commentId') commentId: string, @Request() req) {
+    return this.commentService.deleteComment(req.user.id, commentId);
   }
 
-  // 좋아요 버튼을 눌렀을 때
-  @Get('like/:commentId')
-  clickLikeButton() {
-    return 'click like button';
+  // 좋아요 버튼을 눌렀을 때s
+  @Post('like/:commentId')
+  clickLikeButton(@Param('commentId') commentId: string, @Request() req) {
+    return this.commentService.clickLiketButton(req.user.id, commentId);
   }
 
   // 싫어요 버튼을 눌렀을 때
-  @Get('dislike/:commentId')
-  clickDislikeButton() {
-    return 'click dislike button';
+  @Post('dislike/:commentId')
+  clickDislikeButton(@Param('commentId') commentId: string, @Request() req) {
+    return this.commentService.clickDislikstButton(req.user.id, commentId);
   }
 }
