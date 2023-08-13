737d3c47653d022e37658486139ad93fc7871b9e
novice0840 <magnolia0840@gmail.com>
2023-08-13 18:54:16 +0900

    Naver webtoon cralwing refactoring

--- backend/src/cronjob/cronjob.controller.ts
+++ backend/src/cronjob/cronjob.controller.ts
@@ -1,27 +1,38 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CronjobService } from './cronjob.service';
+import { NaverCrawlerService } from 'src/crawler/navercrawler.service';
 
 @Controller('cronjob')
 export class CronjobController {
-  constructor(private readonly cronjobService: CronjobService) {}
+  constructor(
+    private readonly cronjobService: CronjobService,
+    private readonly naverCrawlerService: NaverCrawlerService,
+  ) {}
 
-  @Get('/init/all')
-  initAll() {
-    return this.cronjobService.initAll();
+  @Get('test')
+  async test() {
+    // const webtoons = await this.naverCrawlerService.cralwingWebtoonBase();
+    const webtoons = [{ code: '783053' }];
+    return this.naverCrawlerService.crawlingChapter(webtoons);
   }
 
-  @Get('init/webtoons')
-  initWebtoons() {
-    return 'init all webtoon';
-  }
+  // @Get('init/all')
+  // initAll() {
+  //   return this.cronjobService.initAll();
+  // }
 
-  @Get('/init/dayofweek/:dayofweek')
-  initDayOfWeek(@Param('dayofweek') dayofweek: string) {
-    return this.cronjobService.initDayOfWeek(dayofweek);
-  }
+  // @Get('init/webtoons')
+  // initWebtoons() {
+  //   return 'init all webtoon';
+  // }
 
-  @Get('/init/webtoon/:webtoonId')
-  testWebtoon(@Param('webtoonId') webtoonId) {
-    return this.cronjobService.initWebtoon(webtoonId);
-  }
+  // @Get('init/dayofweek/:dayofweek')
+  // initDayOfWeek(@Param('dayofweek') dayofweek: string) {
+  //   return this.cronjobService.initDayOfWeek(dayofweek);
+  // }
+
+  // @Get('init/webtoon/:webtoonId')
+  // testWebtoon(@Param('webtoonId') webtoonId) {
+  //   return this.cronjobService.initWebtoon(webtoonId);
+  // }
 }
