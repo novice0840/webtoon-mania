130f8f5daa3257db30d1e37ca6003a5807870f8b
novice0840 <magnolia0840@gmail.com>
2023-09-03 03:44:35 +0900

    투믹스 웹툰 크롤링

--- backend/src/crawler/toomicscralwer.service.ts
+++ backend/src/crawler/toomicscralwer.service.ts
@@ -5,7 +5,69 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ToomicsCrawlerService {
-  crawlingWebtoons() {
-    return 'toomics';
+  private async autoScroll(page): Promise<void> {
+    await page.evaluate(async () => {
+      await new Promise<void>((resolve) => {
+        let totalHeight = 0;
+        const distance = 100;
+        const timer = setInterval(() => {
+          const scrollHeight = document.body.scrollHeight;
+          window.scrollBy(0, distance);
+          totalHeight += distance;
+          if (totalHeight >= scrollHeight - window.innerHeight) {
+            clearInterval(timer);
+            resolve();
+          }
+        }, 10);
+      });
+    });
+  }
+
+  async crawlingCurrentWebtoons() {
+    const webtoons = [];
+    const days = [1]; // [1, 2, 3, 4, 5, 6, 7];
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    for await (const day of days) {
+      await page.goto(`https://www.toomics.com/webtoon/weekly/dow/${day}`);
+      await page.waitForSelector('li.grid__li');
+      await this.autoScroll(page);
+      const content = await page.content();
+      const $ = load(content);
+      $('li.grid__li').each((index, element) => {
+        const thumbnail = $(element).find('img').attr('src');
+        const titleId = $(element).find('a').attr('href')?.split('/')[6];
+        const titleName = $(element).find('span.toon-dcard__link').text();
+        const link = `https://www.toomics.com/webtoon/episode/toon/${titleId}`;
+        webtoons.push({ thumbnail, titleId, titleName, link, isEnd: false, platform: 'toomics' });
+      });
+      console.log(`${day} 연재 웹툰 크롤링 완료`);
+    }
+    await browser.close();
+    return webtoons;
+  }
+
+  async crawlingEndWebtoons() {
+    const webtoons = [];
+    const days = [1]; // [1, 2, 3, 4, 5, 6, 7];
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    for await (const day of days) {
+      await page.goto(`https://www.toomics.com/webtoon/weekly/dow/${day}`);
+      await page.waitForSelector('li.grid__li');
+      await this.autoScroll(page);
+      const content = await page.content();
+      const $ = load(content);
+      $('li.grid__li').each((index, element) => {
+        const thumbnail = $(element).find('img').attr('src');
+        const titleId = $(element).find('a').attr('href')?.split('/')[6];
+        const titleName = $(element).find('span.toon-dcard__link').text();
+        const link = `https://www.toomics.com/webtoon/episode/toon/${titleId}`;
+        webtoons.push({ thumbnail, titleId, titleName, link, isEnd: false, platform: 'toomics' });
+      });
+      console.log(`${day} 연재 웹툰 크롤링 완료`);
+    }
+    await browser.close();
+    return webtoons;
   }
 }
--- backend/src/crawler/toptooncrawler.service.ts
+++ backend/src/crawler/toptooncrawler.service.ts
@@ -62,7 +62,6 @@ export class ToptoonCrawlerService {
         }
       }
     }
-    return '';
   }
 
   async crawlingCurrentWebtoons() {
@@ -91,14 +90,14 @@ export class ToptoonCrawlerService {
       });
       console.log(`${day} 연재 웹툰 크롤링 완료`);
     }
-
+    await browser.close();
     return webtoons;
   }
 
   async crawlingEndWebtoons() {
-    const years = [2023]; //[2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];
+    const years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];
     const webtoons = [];
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: 'new' });
     const page = await browser.newPage();
     for await (const year of years) {
       await page.goto(`https://toptoon.com/complete#complete3`);
@@ -116,14 +115,18 @@ export class ToptoonCrawlerService {
           ?.split('url')[1]
           .slice(1, -2)
           .replace(/^"|"$/g, '');
-        const titleId = $(element).find('a').attr('href')?.split('/')[3];
-        const titleName = $(element).find('span.thumb_tit_text').text();
-        const viewCount = this.convertToNumber($(element).find('span.viewCountTxt').text());
-        const link = `https://toptoon.com/comic/ep_list/${titleId}`;
-        webtoons.push({ thumbnail, titleId, titleName, viewCount, link, isEnd: true, platform: 'toptoon' });
+        // 탑툰이 특정 탭을 눌러 웹툰을 가져올때 그 다음 탭의 웹툰을 일부분을 미리 불러오기 때문에 해당 부분은 썸네일이 없다.
+        if (thumbnail) {
+          const titleId = $(element).find('a').attr('href')?.split('/')[3];
+          const titleName = $(element).find('span.thumb_tit_text').text();
+          const viewCount = this.convertToNumber($(element).find('span.viewCountTxt').text());
+          const link = `https://toptoon.com/comic/ep_list/${titleId}`;
+          webtoons.push({ thumbnail, titleId, titleName, viewCount, link, isEnd: true, platform: 'toptoon' });
+        }
       });
       console.log(`${year}년도 완결 웹툰 크롤링 완료`);
     }
+    await browser.close();
     return webtoons;
   }
 
@@ -152,6 +155,7 @@ export class ToptoonCrawlerService {
     const day_of_weeks = [];
     $('.comic_tag span').each((index, element) => {
       const tag = $(element).text().slice(1);
+
       if (days.includes(tag)) {
         day_of_weeks.push(dayConverter[tag]);
       } else {
@@ -159,6 +163,7 @@ export class ToptoonCrawlerService {
       }
     });
     const starScore = parseFloat($('.comic_spoint').text());
+    await browser.close();
     return { authors, description, tags, day_of_weeks, starScore };
   }
 }
