7876bdc4daec61ad00464220ecce2d198ede9327
novice0840 <magnolia0840@gmail.com>
2023-10-12 01:43:05 +0900

    절대경로 설정 변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,37 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { resolve } from "path";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
-    alias: [
-      { find: "@src", replacement: resolve(__dirname, "src") },
-      {
-        find: "@components",
-        replacement: resolve(__dirname, "src/components"),
-      },
-      {
-        find: "@api",
-        replacement: resolve(__dirname, "src/api"),
-      },
-      {
-        find: "@utils",
-        replacement: resolve(__dirname, "src/utils"),
-      },
-      {
-        find: "@hooks",
-        replacement: resolve(__dirname, "src/hooks"),
-      },
-      {
-        find: "@pages",
-        replacement: resolve(__dirname, "src/pages"),
-      },
-      {
-        find: "@types",
-        replacement: resolve(__dirname, "src/types"),
-      },
-    ],
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
   },
 });
