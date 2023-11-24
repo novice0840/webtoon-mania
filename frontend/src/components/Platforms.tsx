95af2fd7ee0d6e0a0880d95ce1031a8ab41f5e6b
novice0840 <magnolia0840@gmail.com>
2023-11-24 20:15:54 +0900

    feat: 장르 필터 기능 완성

--- frontend/src/components/Platforms.tsx
+++ frontend/src/components/Platforms.tsx
@@ -4,25 +4,57 @@ import { useSearchParams } from "react-router-dom";
 
 const Platforms = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [tab, setTab] = useState(0);
+  const platform = searchParams.get("platform");
+
+  const platformConverter = (platform: string | null) => {
+    if (platform === "naver") {
+      return 1;
+    } else if (platform === "kakao") {
+      return 2;
+    } else if (platform === "lezhin") {
+      return 3;
+    } else if (platform === "toptoon") {
+      return 4;
+    } else if (platform === "toomics") {
+      return 5;
+    } else {
+      return 0;
+    }
+  };
+
+  const [tab, setTab] = useState(platformConverter(platform));
   const genres = searchParams.getAll("genres");
   const dayOfWeeks = searchParams.getAll("dayOfWeeks");
+  const isEnd = searchParams.get("isEnd");
 
   const handleTabs = (event: React.SyntheticEvent, newTab: number) => {
-    setTab(newTab);
-    if (newTab === 0) {
-      setSearchParams({ genres, dayOfWeeks });
-    } else if (newTab === 1) {
-      setSearchParams({ genres, dayOfWeeks, platform: "naver" });
-    } else if (newTab === 2) {
-      setSearchParams({ genres, dayOfWeeks, platform: "kakao" });
-    } else if (newTab === 3) {
-      setSearchParams({ genres, dayOfWeeks, platform: "lezhin" });
-    } else if (newTab === 4) {
-      setSearchParams({ genres, dayOfWeeks, platform: "toptoon" });
-    } else if (newTab === 5) {
-      setSearchParams({ genres, dayOfWeeks, platform: "toomics" });
+    let newParams = {};
+
+    if (isEnd) {
+      newParams = { ...newParams, isEnd };
     }
+
+    if (newTab === 0) {
+      setTab(0);
+      newParams = { ...newParams, genres, dayOfWeeks };
+    } else if (newTab === 1) {
+      setTab(1);
+      newParams = { ...newParams, genres, dayOfWeeks, platform: "naver" };
+    } else if (newTab === 2) {
+      setTab(2);
+      newParams = { ...newParams, genres, dayOfWeeks, platform: "kakao" };
+    } else if (newTab === 3) {
+      setTab(3);
+      newParams = { ...newParams, genres, dayOfWeeks, platform: "lezhin" };
+    } else if (newTab === 4) {
+      setTab(4);
+      newParams = { ...newParams, genres, dayOfWeeks, platform: "toptoon" };
+    } else if (newTab === 5) {
+      setTab(5);
+      newParams = { ...newParams, genres, dayOfWeeks, platform: "toomics" };
+    }
+
+    setSearchParams(newParams);
   };
 
   return (
