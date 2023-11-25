6589706ecdc0cfc073a3cf3d515d782504bdd07c
novice0840 <magnolia0840@gmail.com>
2023-11-26 08:54:00 +0900

    feat: 무한 스크롤 구현완료

--- frontend/src/components/WebtoonList.tsx
+++ frontend/src/components/WebtoonList.tsx
@@ -1,28 +1,60 @@
-import React, { ForwardedRef, forwardRef, useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Grid, Paper, Typography, Link, Stack } from "@mui/material";
 import { WebtoonBaseType } from "@src/types";
 import { Box } from "@mui/material";
-import { genres, webtoons } from "@src/utils/constants";
 import { useSearchParams } from "react-router-dom";
 
 const WebtoonList = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [webtoons, setWebtoons] = useState<WebtoonBaseType[]>([]);
-  const lastWebtoonRef = useRef<HTMLElement>(null);
+  const [totalPage, setTotalPage] = useState<number>(0);
+  const [curPage, setCurPage] = useState<number>(1);
+  const lastWebtoonRef = useRef<Element>(null);
   const platform = searchParams.get("platform");
   const genres = searchParams.getAll("genres").toString();
   const dayOfWeeks = searchParams.getAll("dayOfWeeks").toString();
   const isEnd = searchParams.get("isEnd") ? true : false;
+  // let isLoading = false;
 
+  // 첫 페이지 로딩
   useEffect(() => {
     void fetch(
       `${import.meta.env.VITE_API_BASE_URL as string}/webtoon/list${window.location.search}`
     )
       .then((res) => res.json())
       .then((data: { totalPage: number; page: number; data: WebtoonBaseType[] }) => {
+        setCurPage(1);
+        setTotalPage(data.totalPage);
         setWebtoons(data.data);
       });
   }, [platform, genres, dayOfWeeks, isEnd]);
+
+  // 추가 페이지 로딩
+  useEffect(() => {
+    if (curPage === 1 || totalPage === 0 || curPage >= totalPage) return;
+    void fetch(
+      `${import.meta.env.VITE_API_BASE_URL as string}/webtoon/list${window.location.search}${
+        window.location.search === "" ? `?page=${curPage}` : `&page=${curPage}`
+      }`
+    )
+      .then((res) => res.json())
+      .then((data: { totalPage: number; page: number; data: WebtoonBaseType[] }) => {
+        setWebtoons([...webtoons, ...data.data]);
+      });
+  }, [curPage]);
+
+  useEffect(() => {
+    if (!lastWebtoonRef.current) return;
+    const observer = new IntersectionObserver((entries: any) => {
+      if (lastWebtoonRef.current && entries[0].isIntersecting) {
+        console.log("intersecting");
+        setCurPage((curPage) => curPage + 1);
+      }
+    });
+
+    observer.observe(lastWebtoonRef.current);
+    return () => observer.disconnect();
+  }, []);
 
   return (
     <Grid container rowSpacing={5} spacing={3}>
@@ -37,6 +69,7 @@ const WebtoonList = () => {
           </Link>
         </Grid>
       ))}
+      {<Box ref={lastWebtoonRef}></Box>}
     </Grid>
   );
 };
