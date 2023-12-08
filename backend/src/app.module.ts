6765c2ef433eb30a2a709d4ad1b1255df0fd4b15
novice0840 <magnolia0840@gmail.com>
2023-12-09 00:12:25 +0900

    fix: test 용 파일 제거

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,23 +8,24 @@ import { AuthModule } from './auth/auth.module';
 import { ChattingModule } from './chatting/chatting.module';
 import { UserModule } from './user/user.module';
 import { CommentModule } from './comment/comment.module';
-import { Chatting, Comment, User, Webtoon, Like, Dislike, Author, DayOfWeek, Genre } from 'src/entity';
 import { CrawlerModule } from './crawler/crawler.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      // entities: [Chatting, Comment, User, Webtoon, Like, Dislike, Author, DayOfWeek, Genre],
-      entities: ['src/entity/**/*.ts'],
-      autoLoadEntities: true,
-      synchronize: true,
-      // logging: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: ['./entity/**/*.entity.ts'],
+        autoLoadEntities: true,
+        synchronize: true,
+        // logging: true,
+      }),
     }),
     ScheduleModule.forRoot(),
     WebtoonModule,
