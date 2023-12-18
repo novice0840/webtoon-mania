aec06c3c909fcdf4bc0a6eb9d92b734a4e0974df
novice0840 <magnolia0840@gmail.com>
2023-12-18 16:58:22 +0900

    feat: 웹툰 API swagger 설명 추가

--- backend/src/modules/webtoon/webtoon.controller.ts
+++ backend/src/modules/webtoon/webtoon.controller.ts
@@ -1,28 +1,28 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { WebtoonService } from './webtoon.service';
 import { WebtoonListQueryDTO, WebtoonDetailDTO, WebtoonListDTO } from 'src/modules/webtoon/dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiResponse, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('webtoon')
 @ApiTags('Webtoon')
 export class WebtoonController {
   constructor(private readonly webtoonService: WebtoonService) {}
 
-  // 여러 웹툰들을 배열로 요청
   @Get('/list')
+  @ApiOkResponse({ description: '웹툰 목록 불러오기 성공', type: WebtoonListDTO })
   async getWebtoonAll(@Query() query: WebtoonListQueryDTO): Promise<WebtoonListDTO> {
     return this.webtoonService.getWebtoonList(query);
   }
 
-  // 특정 한 웹툰의 세부 정보
   @Get('/detail/:id')
-  getWebtoonOne(@Param('id') id: string) {
+  @ApiOkResponse({ description: '웹툰 상세 정보 불러오기 성공', type: WebtoonDetailDTO })
+  getWebtoonOne(@Param('id') id: string): Promise<WebtoonDetailDTO> {
     return this.webtoonService.getOneWebtoon(id);
   }
 
-  // 웹툰 인기 장르 50개 반환
   @Get('/kinds')
-  getWebtoonKinds() {
+  @ApiOkResponse({ description: '인기 장르 50개 불러오기 성공' })
+  getWebtoonKinds(): Promise<string[]> {
     return this.webtoonService.getWebtoonKinds();
   }
 }
