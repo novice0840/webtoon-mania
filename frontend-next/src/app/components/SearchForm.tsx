0020a53199f10ca0eef0ae22506c115e99f4622b
novice0840 <magnolia0840@gmail.com>
2025-01-03 04:55:31 +0900

    feat: SearchForm Value 추출 완료

--- frontend-next/src/app/components/SearchForm.tsx
+++ frontend-next/src/app/components/SearchForm.tsx
@@ -1,11 +1,21 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import { FormEvent } from "react";
 
 const SearchForm = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target as HTMLFormElement);
+    const searchValue = formData.get("search");
+
+    console.log("Input value:", searchValue);
+  };
+
   return (
-    <form className="flex gap-4">
-      <Input placeholder="검색" />
+    <form onSubmit={handleSubmit} className="flex gap-4">
+      <Input name="search" placeholder="검색" />
       <Button>확인</Button>
     </form>
   );
