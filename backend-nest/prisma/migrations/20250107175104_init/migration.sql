fc7ab5ce88e8d9cab330a012111c59b4f1dd0c2c
novice0840 <magnolia0840@gmail.com>
2025-01-08 07:09:07 +0900

    feat: DB schema 수정

--- backend-nest/prisma/migrations/20250107175104_init/migration.sql
+++ backend-nest/prisma/migrations/20250107175104_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
+    `sample` VARCHAR(191) NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
@@ -15,6 +16,15 @@ CREATE TABLE `Post` (
     `content` VARCHAR(191) NULL,
     `published` BOOLEAN NULL DEFAULT false,
     `authorId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Sample` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+    `userId` INTEGER NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
