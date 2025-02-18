2185364ec18ad2553e75dfa27cf39f9862efb926
novice0840 <magnolia0840@gmail.com>
2025-02-19 04:09:22 +0900

    refactor: frontend next로 변경

--- frontend/src/api/init.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const axiosInstace = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL as string,
-});
-
-export default axiosInstace;
--- frontend/src/pages/UserInfoPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import MainLayout from "@src/layout/MainLayout";
-
-const UserInfoPage = () => {
-  return <MainLayout>this is user info page</MainLayout>;
-};
-
-export default UserInfoPage;
