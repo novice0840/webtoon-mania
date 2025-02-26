d54b48da110872088e474eb41d8863add6a08342
novice0840 <magnolia0840@gmail.com>
2025-02-26 14:31:03 +0900

    docs: platform api swagger 문서 추가

--- backend/src/modules/platforms/platforms.controller.ts
+++ backend/src/modules/platforms/platforms.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { PlatformsService } from './platforms.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('platforms')
 export class PlatformsController {
   constructor(private readonly platformService: PlatformsService) {}
 
   @Get()
+  @ApiOperation({
+    summary: '플랫폼 목록 조회',
+  })
   async getPlatforms() {
     return await this.platformService.getPlatforms();
   }
