931493b4739d8583d738db03fd3b13ba44ef888d
novice0840 <magnolia0840@gmail.com>
2025-02-27 10:28:51 +0900

    feat: swagger 예제 example 추가

--- backend/src/modules/genres/genres.controller.ts
+++ backend/src/modules/genres/genres.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { GenresService } from './genres.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('genres')
 export class GenresController {
@@ -9,6 +9,13 @@ export class GenresController {
   @Get()
   @ApiOperation({
     summary: '장르 목록 조회',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공',
+    schema: {
+      example: ['로맨스', '판타지', '드라마', '스릴러', '액션', '코미디'],
+    },
   })
   async getGenres() {
     return await this.genresService.getGenres();
--- backend/src/modules/platforms/platforms.controller.ts
+++ backend/src/modules/platforms/platforms.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { PlatformsService } from './platforms.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('platforms')
 export class PlatformsController {
@@ -9,6 +9,19 @@ export class PlatformsController {
   @Get()
   @ApiOperation({
     summary: '플랫폼 목록 조회',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공',
+    schema: {
+      example: [
+        '네이버웹툰',
+        '카카오페이지',
+        '다음웹툰',
+        '레진코믹스',
+        '코미코',
+      ],
+    },
   })
   async getPlatforms() {
     return await this.platformService.getPlatforms();
