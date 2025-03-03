0aa325a002d88b7b950c7e720f94b5eb876209a7
novice0840 <magnolia0840@gmail.com>
2025-03-03 19:46:30 +0900

    feat: platform list 내림차순으로 변경

--- backend/src/modules/platforms/platforms.service.ts
+++ backend/src/modules/platforms/platforms.service.ts
@@ -6,8 +6,24 @@ export class PlatformsService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async getPlatforms() {
-    return (await this.prismaService.platform.findMany()).map(
-      ({ name }) => name,
-    );
+    const platformCount: Record<string, number> = {};
+    const webtoonPlatforms = await this.prismaService.webtoonPlatform.findMany({
+      include: {
+        platform: true,
+      },
+    });
+
+    webtoonPlatforms.forEach((webtoonPlatform) => {
+      const platformName = webtoonPlatform.platform.name;
+      if (platformName in platformCount) {
+        platformCount[platformName] += 1;
+      } else {
+        platformCount[platformName] = 0;
+      }
+    });
+
+    return Object.entries(platformCount)
+      .sort(([, valueA], [, valueB]) => valueB - valueA)
+      .map(([key]) => key);
   }
 }
