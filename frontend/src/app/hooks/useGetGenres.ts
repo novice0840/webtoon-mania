e4d01c5f4fd72f1b4b1e664e6c5112c8a3936118
novice0840 <magnolia0840@gmail.com>
2025-03-04 20:10:41 +0900

    refactor: 불필요한 코드 제거

--- frontend/src/app/hooks/useGetGenres.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import Fetch from "@/app/utils/fetch";
-
-export const useGetGenres = () => {
-  return useQuery<string[]>({
-    queryKey: ["useGetGenres"],
-    queryFn: () => Fetch.get(`genres`),
-  });
-};
--- frontend/src/app/hooks/useGetPlatforms.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import Fetch from "@/app/utils/fetch";
-
-export const useGetPlatforms = () => {
-  return useQuery<string[]>({
-    queryKey: ["useGetPlatform"],
-    queryFn: () => Fetch.get(`platforms`),
-  });
-};
