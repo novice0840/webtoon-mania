c405cf947994b61088a4cea58a34c701274c7829
novice0840 <magnolia0840@gmail.com>
2025-04-13 21:04:47 +0900

    feat: create user DTO 추가

---
+++ backend/src/modules/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/createUser.dto';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(dto: CreateUserDto): Promise<User> {
+    const existing = await this.prismaService.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (existing) {
+      throw new ConflictException('이미 가입된 이메일입니다.');
+    }
+
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email: dto.email,
+        password: hashedPassword,
+      },
+    });
+
+    return user;
+  }
+}
